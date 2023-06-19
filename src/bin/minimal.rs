b8df52a2c83b382be7326c3e33f35a1c8b3906f9
s7rul <eriser-8@student.ltu.se>
2023-06-19 18:45:52 +0200

    still compiler error

--- src/bin/minimal.rs
+++ src/bin/minimal.rs
@@ -11,30 +11,18 @@ use rp2040_rtfmt as _;
 mod app {
     use rp_pico::hal::{
         clocks, gpio,
-        gpio::pin::bank0::{Gpio2, Gpio25, Gpio3},
+        gpio::pin::bank0::{Gpio25},
         gpio::pin::PushPullOutput,
-        pac,
         sio::Sio,
         watchdog::Watchdog,
-        I2C,
     };
     use rp_pico::XOSC_CRYSTAL_FREQ;
 
-    use core::mem::MaybeUninit;
     use embedded_hal::digital::v2::{OutputPin, ToggleableOutputPin};
-    use fugit::RateExtU32;
     use rtic_monotonics::rp2040::*;
 
 
     use panic_probe as _;
-
-    type I2CBus = I2C<
-        pac::I2C1,
-        (
-            gpio::Pin<Gpio2, gpio::FunctionI2C>,
-            gpio::Pin<Gpio3, gpio::FunctionI2C>,
-        ),
-    >;
 
     #[shared]
     struct Shared {}
@@ -42,14 +30,12 @@ mod app {
     #[local]
     struct Local {
         led: gpio::Pin<Gpio25, PushPullOutput>,
-        i2c: &'static mut I2CBus,
     }
 
     #[init(local=[
         // Task local initialized resources are static
         // Here we use MaybeUninit to allow for initialization in init()
         // This enables its usage in driver initialization
-        i2c_ctx: MaybeUninit<I2CBus> = MaybeUninit::uninit()
     ])]
     fn init(mut ctx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
@@ -83,29 +69,15 @@ mod app {
         led.set_low().unwrap();
 
         // Init I2C pins
-        let sda_pin = gpioa.gpio2.into_mode::<gpio::FunctionI2C>();
-        let scl_pin = gpioa.gpio3.into_mode::<gpio::FunctionI2C>();
-
-        // Init I2C itself, using MaybeUninit to overwrite the previously
-        // uninitialized i2c_ctx variable without dropping its value
-        // (i2c_ctx definined in init local resources above)
-        let i2c_tmp: &'static mut _ = ctx.local.i2c_ctx.write(I2C::i2c1(
-            ctx.device.I2C1,
-            sda_pin,
-            scl_pin,
-            100.kHz(),
-            &mut ctx.device.RESETS,
-            &clocks.system_clock,
-        ));
 
         // Spawn heartbeat task
         heartbeat::spawn().ok();
 
         // Return resources and timer
-        (Shared {}, Local { led, i2c: i2c_tmp })
+        (Shared {}, Local { led})
     }
 
-    #[task(local = [i2c, led])]
+    #[task(local = [led])]
     async fn heartbeat(ctx: heartbeat::Context) {
         // Loop forever.
         //
